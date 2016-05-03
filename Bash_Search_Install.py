45c6f116791cee153110371d7f28e12b821c9dd3
Michael Currin <michael.currin@gmail.com>
2016-05-03 17:48:55 +0200

    Bash_Search_Install.py
    I figured out how to use ";" to call a sequence of commands and implemented that.
    
    I neatened up the code and made some examples to test by removing the #.

--- Bash_Search_Install.py
+++ Bash_Search_Install.py
@@ -4,56 +4,80 @@ Created on Thu Apr 28 10:03:59 2016
 
 @author: michaelcurrin
 
+This script defines a short Console function to pass commands to the Mac / Unix terminal.
+The command line code used in this script includes retrieving folders in the working directory or a subfolder, which match a condition.
+It also allows you to install a number of libraries.
 
 """
 
 import subprocess
 
 def Console(command): 
-    """input instruction for console, run it and save the result in output string."""
+    """take input instruction for console (string), run it in the terminal and save the result in output string."""
     
-    try:     
-        process = subprocess.Popen(command, stdout=subprocess.PIPE) # problem might be process closes after each cmd
-        output = process.communicate()[0]
-    except:
-        output = "Error"
-    
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True) # split turns string into array
+    output = process.communicate()[0]
+
     return output
-
-
-def SearchForFileType(fileExtension):
-    """return an array of filenames which contain fileExtension input (e.g. 'csv'), using Console function. """
     
-    string = Console(['ls']) # get the list of files and and folders in the directory of this Py file.
-    array = string.split() # convert string with \n signs into array
+def SearchFor(fileExtension):
+    """take the input string (e.g. 'csv') and return a list of files and folders which contain it."""
+    
+    string = Console('ls') # get the list of files and and folders, in the directory of this Py file
+    array = string.split() # convert string result with "\n" delimeter into a list
         
     fileArray = []
-
-    for name in array: # loop through names looking for those containing 'csv' and add to array
+    for name in array: # loop through names, looking for those containing fileExtension, then add to array
         if fileExtension in name.lower():
             fileArray.append(name)
+    
     return fileArray   
 
+def SearchInFolder(fileExtension, folderName):
+    """take the fileExtension input string (e.g. 'csv') and search a subfolder (e.g. 'Inputs') and return a list of files and folders which contain the string."""
 
-def InstallModules():
-    """Loop through a list of libraries to install.
-    Attempt to install using Console function and show OK/FAIL result."""
+    string = Console('cd %s; ls' % folderName)  # go to subfolder. Then show a list of files and folders within. The result will be empty if the folder does not exist.
+    array = string.split() # convert string result with "\n" delimeter into a list
+        
+    fileArray = []
+    for name in array: # loop through names looking for those containing fileExtension and add to array
+        if fileExtension in name.lower():
+            fileArray.append(name)
     
-    modules = ['oauth2']    
+    return fileArray  
+
+
+def InstallModules(modules):
+    """Loop through a list of libraries inputted.
+    Attempt to install using Console function.
+    Show OK if installing successfully or previous, othterwise show a FAIL message."""
+    
     for lib in modules:
         install = 'pip install %s' % lib
         try:
-            Console(install) # attempt to install library if not already installed
-            print install + ' . . . OK'
+            Console(install)
+            print install + ' ... OK'
         except:
-            print install + ' . . . FAIL'
-       
-       
-# test #http://stackoverflow.com/questions/8601312/python-passing-multiple-parameters-for-popen-command
-command = ['cd ..', 'pwd'] # cd .. gives an error
-process = subprocess.Popen(command, stdout=subprocess.PIPE) # problem might be process closes after each cmd
-output = process.communicate()[0]   
-print output      
-            
-            
-#print SearchForFileType('.csv') # require full stop otherwise py files containing csv in the name are returned.
+            print install + ' ... FAIL'
+
+
+""" test the Console function """
+cmd = 'ls; pwd; cd ..; pwd' 
+#print Console(cmd)
+
+""" test the Console function """
+cmd = 'pwd; cd Inputs; pwd; ls' # requires Inputs folder to exist in working directory of current Py file
+#print Console(cmd)
+
+""" test the SearchFor function """
+ext = '.csv'
+#print SearchFor(ext)
+
+""" test the SearchInFolder function """
+ext = '.csv'
+subf = 'Inputs'
+#print SearchInFolder(ext, subf)
+
+""" test the InstallModules function """
+modules = ['oauth2', 'Flask']
+#print InstallModules(modules)
