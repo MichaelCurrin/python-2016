711b186e3fa62deafa409579dc43d2eb838c7ab4
Michael Currin <michael.currin@gmail.com>
2016-07-03 22:36:02 +0200

    Clearer comments and tests

--- Bash_Search_Install.py
+++ Bash_Search_Install.py
@@ -6,72 +6,130 @@ Created on Thu Apr 28 10:03:59 2016
 
 @author: michaelcurrin
 
-Bash_Search_Install.py
-This script defines a short Console function to pass commands to the Mac / Unix terminal.
-The command line code used in this script includes retrieving folders in the working directory or a subfolder, which match a condition.
-It also allows you to install a number of libraries.
+This script defines functions used to pass commands to the Mac or Unix
+ terminal, as comand line or bash code. 
+The capabilities are explained within each function.
+
+To see how to run this script in other scripts, 
+    place test_Bash.py and Bash_Search_Install.py in the same directory. 
+    Run testConsole script.
 
 """
 
 import subprocess
 
 def Console(command): 
-    """Take input instruction for console as string. Run it in the terminal. Return the result in output string.
+    """Take input instruction for console as string. 
+    Run it in the terminal. 
+    Return the result in output string.
+    
+    The online Python documentation on the subprocess library includes notes 
+    and cautions on use of shell=True versus shell=False.
 
     Arguments:
         command
-            string to excecute
+            string of commands to excecute, separated by a semi-colon
             e.g. 'ls'
+            e.g. 'pwd'
+            e.g. 'ls; pwd; cd ..; pwd' 
+	Returns:
+		output
+		response from the terminal
     """
     
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True) # split turns string into array
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output = process.communicate()[0]
 
     return output
 
 
-def ConsoleSearch(fileExtension, folderName):
-    """Search a subfolder and return a list of files of folders which contain the input string.
+def ConsoleSearch(searchTerm = '', folderName =''):
+    """
+    Use the console to return a list of files and folders, within an 
+    optionally defined subdirectory. 
+    Otherwise use the current working directory of the script which is running.
+
+    Search for files and folders which contain the inputted search text, or 
+    leave input blank to show all results.
+
     
     Arguments: 
-        fileExtension
-            The string to search for, which could be a part of a filename if it excludes a dot. 
+        searchTerm
+            The string to search for in the file name and/or extension
+            e.g. ''
             e.g. '.csv' 
             e.g. '2016'
+            e.g. ' 2016.xls'
         folderName
-            The name of the subfolder to search within. 
-            Leave blank if not required. 
-            e.g. 'Inputs' 
+            The exact name of the subfolder directory to search within. 
+            e.g. 'Inputs/' 
+            
+            Use a forward slash as below for tree structure.
+            e.g. 'Projects/Completed/'
+            
+            This is optional. You can set as empty string or omit it.
             e.g. ''
+            
+            If the location is not valid, the working directory will be used.
+            
+            Use single quotes within the name, to ensure the terminal 
+            accepts the folder name with its spaces .
+            e.g. "'July 2016'/"
+            
+    Returns
+    	fileArray
+    		list
+    		response from the terminal
+    		names of file or foldernames which match the search parameters
     """
 
+    # check if a subfolder is defined and navigate to it.
+    # show a list of files and folders at the location'. 
     if folderName:
         cmd = 'cd %s; ls' % folderName
     else:
         cmd = 'ls'
-    string = Console(cmd)  # go to subfolder. Then show a list of files and folders within. The result will be empty if the folder does not exist.
-    array = string.split() # convert string result with "\n" delimeter into a list
-        
-    fileArray = []
-    for name in array: # loop through names looking for those containing fileExtension and add to array
-        if fileExtension in name.lower():
-            if folderName: # add path to filenames
-                fileArray.append('%s/%s' % (folderName, name)) # e.g. 'Inputs/authentication.csv'
-            else:
-                fileArray.append(name) # e.g. 'authentication.csv'
+    string = Console(cmd)  
+    # convert string result with "\n" delimeter into a list
+    array = string.split('\n') 
+    
+    # check if a search term filter needs to be applied
+    if searchTerm:
+        fileArray = []
+        # loop through names looking for those containing searchTerm and 
+        # add to array
+        for name in array: 
+            if searchTerm in name.lower():
+	            if folderName: # add path to filenames
+	                fileArray.append('%s/%s' % (folderName, name))
+                     # e.g. 'Inputs/authentication.csv'
+	            else:
+	                fileArray.append(name) 
+                     # e.g. 'authentication.csv'
+    else:
+		fileArray = array
                 
     return fileArray  
 
 
 def ConsoleInstall(libraries):
-    """Loop through a list of library names which have been provided as input.
-    Attempt to install using Console function.
-    Show OK if installed successfully or previously, othterwise show a FAIL message.
+    """
+    Loop through a list of library names which have been provided as input.
+    Attempt to install them by passing 'pip install' and the library name
+    to the Console function defined in this script
+    Show the outcome as OK (installed or already exists) or FAIL.
 
-    Arguments
+    Arguments:
         libraries
-            list of library names to be installed
+            list 
+            library names to be installed
             e.g. ['oauth2', 'Flask']
+    Returns:
+        	installResultList
+             list
+             response from the terminal as libraries attempted to be installed, 
+             along with the outcome for each.
+             The list should contain at least once library name.
     """
     
     installResultList = []
@@ -79,39 +137,53 @@ def ConsoleInstall(libraries):
         installCommand = 'pip install %s' % lib
         try:
             Console(installCommand)
-            installResultList.append('%s ... OK' % installCommand)            
-            #print installCommand + ' ... OK'
+            outcome = 'OK'            
         except:
-            installResultList.append('%s ... OK' % installCommand)            
-            #print installCommand + ' ... FAIL'
-            
+            outcome = 'FAIL'               
+        installResultList.append('%s ... %s' % (installCommand, outcome))     
     return installResultList
 
+
 if __name__ == '__main__': 
-    # run if  Bash_Search_Install.py file is executed directly. ignore if it is imported into another Py file
+    # run code below as a test if Bash_Search_Install.py file is executed 
+    # directly. ignore if it is imported into another Py file
     
-    """ test the Console function with navigating up a level"""
-    test = 'ls; pwd; cd ..; pwd' 
-    print Console(test)
-    print '---'
+    print """1) test the Console function by navigating up a level"""
+    cmd = 'ls; pwd; cd ..; pwd' 
+    print 'Command: %s' % cmd
+    print Console(cmd)
+    print '\n'   
     
-    """ test the Console function with navigating to a subfoler 'Inputs'"""
-    test = 'pwd; cd Inputs; pwd; ls' # requires Inputs folder to exist in working directory of current Py file
-    print Console(test)
+    print """2) test the Console function by navigating down to a subfolder called 'Inputs'"""
+    # requires Inputs folder to exist in working directory of current Py file
+    cmd = 'pwd; cd Inputs; pwd; ls' 
+    print 'Command: %s' % cmd
+    print Console(cmd)
+    print '\n'   
     
-    """ test the ConsoleSearch function without subfolder """
-    ext = '.py'
-    subf = ''
-    print ConsoleSearch(ext, subf)
-    print '---'    
+    print """3) test the ConsoleSearch function, without args, to show all"""
+    print ConsoleSearch()
+    print '\n'   
     
-    """ test the Console function in subfolder"""
-    ext = '.csv'
+    print """4) test the ConsoleSearch function, without a define subfolder"""
+    term = '.py'
+    print 'Term: %s' % term
+    print ConsoleSearch(searchTerm=term)
+    print '\n'    
+    
+    print """5) test the Console function in subfolder. Search for '.csv'"""
+    term = '.csv'
     subf = 'Inputs'
-    print ConsoleSearch(ext, subf)
-    print '---'    
+    print 'Search for %s in %s' % (term, subf)
+    print ConsoleSearch(searchTerm=term, folderName=subf)
+    print '\n'    
     
-    """ test the ConsoleInstallModules function """
+    print """6) test the ConsoleInstallModules function with 2 library names"""
     testList = ['oauth2', 'Flask']
     print ConsoleInstall(testList)
-    print '---'         
+    print '\n'      
+
+    print """7) Print a list of installed packages"""
+    cmd = 'pip list'    
+    print Console(cmd)
+    print '\n'     
