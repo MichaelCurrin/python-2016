bd2ddea553ebb463c3567a2981af75d96bdbaaef
Michael Currin <michael.currin@gmail.com>
2016-11-02 21:18:19 +0200

    update

--- quick_commands.sql
+++ quick_commands.sql
@@ -8,15 +8,43 @@ I tested them on MySQLWorkbench*/
 SELECT COUNT(*) FROM tbl_company
 
 
+SELECT SUM(TABLE_ROWS) 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_SCHEMA = 'db_cipla_test3';
+
+
 -- ####################
 -- selecting a database
 -- ####################
 
 -- double-click on db name to select as default
 -- OR "USE"
-USE db_yap_connectors_cipla;
+USE db_connectors_cipla;
 SELECT COUNT(*) from information_schema.tables
 WHERE table_type = 'base table'
+
+
+-- ####################
+-- Get Columns
+-- ####################
+
+-- get distinct list of columns
+SELECT DISTINCT (COLUMN_NAME)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA='db_connectors_cipla'
+    ORDER BY COLUMN_NAME;
+    
+
+-- ####################
+-- Count rows in tables
+-- ####################
+
+-- show number of rows in each table for a DB
+
+SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_ROWS 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE  TABLE_SCHEMA LIKE 'db_cipla_test_' and TABLE_ROWS > 0 
+ORDER BY `TABLES`.`TABLE_NAME` ASC, `TABLES`.`TABLE_SCHEMA`  ASC
 
 -- ####################
 -- show variables
@@ -34,5 +62,19 @@ SELECT user,host FROM mysql.user;
 
 -- Grants
 SHOW GRANTS;
--- GRANT ALL PRIVILEDGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
--- GRANT PROX ON "@" TO 'root'@'localhost' WITH GRANT OPTION
+GRANT ALL PRIVILEDGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
+GRANT PROX ON "@" TO 'root'@'localhost' WITH GRANT OPTION
+
+
+-- ####################
+-- Get date range
+-- ####################
+
+-- Show the first and last dates in a table's timestamp column
+
+SELECT 
+MIN(timestamp) AS 'Earliest date',
+MAX(timestamp) AS 'Latest date'	
+FROM tbl_socialposts; 
+
+
