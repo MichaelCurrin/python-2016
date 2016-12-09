3ca5fc7853f36c669e4d823ed29ac4359d85344b
Michael Currin <michael.currin@gmail.com>
2016-12-09 15:55:58 +0200

    refined features
    added additional Google error handling, debugging output (quiet) and refined empty tables

--- upload_to_spreadsheets.py
+++ upload_to_spreadsheets.py
@@ -28,19 +28,8 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-# script configuration use in Main()
-import config
-
-SCOPES = config.SCOPES
-CLIENT_SECRET_FILE = config.CLIENT_SECRET_FILE
-APPLICATION_NAME = config.APPLICATION_NAME
-
-# this was in the original script
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+# handle Google Sheet connection error
+from googleapiclient import errors as google_errors
 
 ###############################################################################
 # Table
@@ -58,18 +47,15 @@ def ReadCSV(fileName):
                      ['A2', 'B2', 'C2']]
     """
     data = []
-    try:
-        # 'rU' is needed because of errors reading some characters
-        # U is for universal
-        with open(fileName, 'rU') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                data.append(row)
-    except:
-        print 'Error reading file: %s' % fileName
+    # 'rU' is needed because of errors reading some characters
+    # U is for universal
+    with open(fileName, 'rU') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row)
     return data
 
-def MakeEmptyTable(full_table):
+'''def MakeEmptyTable(full_table):
     """
     Receive a table with values and return a version of the table with all
     values set to blank ('').
@@ -80,7 +66,7 @@ def MakeEmptyTable(full_table):
                                ['235235'],
                                ['325235235','dgdfg','fdgfdg']
     Returns
-        emptyValues: 2D list of rows and columns set to ''.
+        blank_table: 2D list of rows and columns set to ''.
                      e.g.  [['', '', ''],
                             [''],
                             ['', '', '']]
@@ -88,8 +74,49 @@ def MakeEmptyTable(full_table):
     blank_table = []
     for row in full_table:
         blank_row = ['']*len(row)
-        empty_values.append(blank_row)
-    return empty_values
+        blank_table.append(blank_row)
+    return blank_table'''
+
+def MakeEmptyTable(in_table=[[]], row_count=0, column_count=0):
+    """
+    1 Read in *in_table*
+    2 Create an empty table 
+        of '' values,
+        which has with the same number of rows and columns as the table,
+        (where columns based on the first row).
+    3 If the user has specified *row_count* and/or *column_count*,
+        then these will be used place of the dimensions of *in_table*.
+    
+    Therefore *in_table* can be omitted from input if BOTH row_count and 
+    column_count are set.
+         e.g. MakeEmptyTable(row_count=1000, column_count=200)
+
+    And if *in_table* is used, then the other 2 inputs are optional.
+        e.g. MakeEmptyTable(CSVdata, row_count=1000)
+            will use column_count of CSVdata but row_count as 1000
+
+    Args
+        in_table: <type 'list'> 2D table as list of lists, which may contain
+                    table data in rows and columns.
+                    e.g.    [['abc','def','hij'],
+                            ['232','965','TES']
+                            ['235','768','QWE']]
+        row_count: <type 'int'> number of empty rows to create.
+                    If this is not set, then in_table row count will be used.
+        column_count: <type 'int'> number of empty columns to create in each
+                        row.
+                    If this is not set, then column count of *in_table* first
+                        row  will be used.
+    """
+    if not row_count:
+        row_count = len(in_table) # length of table
+
+    if not column_count:
+        column_count = len(in_table[0]) # length of first row of table
+
+    row_contents = ['']*column_count # repeat '' for X columns
+    blank_table = [row_contents]*row_count # repeat blank row for Y rows
+    return blank_table
 
 ###############################################################################
 # Authorise Sheets API
@@ -100,7 +127,8 @@ def GetCredentials():
     Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+    the OAuth2 flow is completed to obtain the new credentials
+    are store them in hidden .credentials folder.
 
     Returns:
         credentials, the obtained credentials.
@@ -138,50 +166,77 @@ def GetService():
 # Read and write data with Sheets API
 #==============================================================================
 
-def GetGSheetsRange(service, spreadsheetId, range_name, printData=False):
+def GetGSheetsRange(service, spreadsheetId, range_name, test_print_rows=0,
+                    quiet=True):
     """Read data fom Google Spreadsheets using API.
 
     Args
         spreadsheet_ID: string. The long id taken from the URL of the file.
         range_name: string. Name of the sheet and range.
                     e.g. 'Sheet1!A1:E'
+        test_print_rows: integer. 
+            e.g. 0 to not print (default) 
+            e.g. 10 to print 10 rows
     Returns
         values: list of lists. The rows and columns read from the sheet.
     """
-    service = GetService()
+    
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheetId, range=range_name).execute()
 
     values = result.get('values', [])
 
-    print 'READ RESULT'
-    print
-    for k, v in result.iteritems():
-        if k!='values':
-            print '%s: %s' % (k, v)
-
+    if not quiet:
+        print '  READ RESULT'
+        for k, v in result.iteritems():
+            if k is not 'values':
+                print '   %s: %s' % (k, v)
+    """
+    # Example
+    range: tb_staging_camp_perf!A1:Q1000
+    majorDimension: ROWS
+    152 rows returned  
+    """
     if values:
         print '%i rows returned' % len(values)
     else:
         print 'No data returned!'
     print
-    print
 
-    if printData:
-        print 'returned data...'
-        print
-        print range_name
-        print '='*len(range_name)
-        for row in values:
-            for column in row:
-                print column,
+    if not quiet:
+        if test_print_rows:
+            print_values = values[:test_print_rows]
+            print 'printing %i rows...' % len(print_values)
             print
-        print
-
+            print range_name # e.g. tb_staging_camp_day!A1:Z1000
+            print '='*len(range_name)
+            for row in print_values:
+                print ','.join(row)
+            print
     return values
 
+def InsertGSheetsRows(service, spreadsheetId, range_name, values,
+                      value_input_option='RAW', quiet=True):
+    """Insert or append data to Google Spreadsheets using API.
+
+    Args
+        service: authorised Sheets API object
+        spreadsheet_ID: id of the docment to write to
+        range_name: sheet name and cell reference if needed
+        values: list of row and column values to write
+        value_input_option: string. optional.
+            either 'RAW' for conveting numbers and dates to text using '
+    """
+    body = {
+      'values': values
+    }
+    result = service.spreadsheets().values().append(
+                    spreadsheetId=spreadsheetId, range=range_name,
+                    valueInputOption=value_input_option, body=body).execute()
+    print result
+
 def WriteGSheetsRange(service, spreadsheetId, range_name, values,
-                      value_input_option='RAW'):
+                      value_input_option='RAW', quiet=True):
     """Write data to Google Spreadsheets using API.
 
     Args
@@ -198,29 +253,57 @@ def WriteGSheetsRange(service, spreadsheetId, range_name, values,
         spreadsheetId=spreadsheetId, range=range_name,
         valueInputOption=value_input_option, body=body).execute()
 
-    print 'WRITE RESULT'
-    print
-    for k, v in result.iteritems():
-        print '%s: %s' % (k, v)
-    print
-    print
+    if not quiet:
+        print '  WRITE RESULT'
+        for k, v in result.iteritems():
+            print '    %s: %s' % (k, v)
+        print
+    """
+    # example
+    spreadsheetId: 1d9Cb9-_-XTYHevDHUfVvQAZjJY9EN6yeZmEcR4DsFVw
+    updatedRange: tb_staging_camp_day!A1:O630
+    updatedCells: 9450
+    updatedRows: 630
+    updatedColumns: 15
+    """
 
-def OverwriteCells(service, spreadsheetId, sheet, newValues):
+def OverwriteCells(service, spreadsheetId, sheet, newValues, quiet=True):
     """
     Read all cells in a sheet
-    Overwrite them with blank values
+    Overwrite them with blank values to ensure sheet is empty
     Write actual values.
-    This prevents old values staying on the bottom part of thh sheet when
+
+    This prevents old values staying on the bottom part of the sheet when
     only a few new rows are written at the top.
+    Deleting rows or columns would unfortunately references from other sheets
+    to that one, plus arrangment of other cells on that sheeg.
     """
-    existingValues = GetGSheetsRange(service, spreadsheetId, sheet)
-    emptyValues = MakeEmptyTable(existingValues)
-    WriteGSheetsRange(service, spreadsheetId, sheet, emptyValues)
-    WriteGSheetsRange(service, spreadsheetId, sheet, newValues,
-                       value_input_option='USER_ENTERED')
+    print 'Sheet:  %s' % sheet
+    print 'Command:  Upload to sheets, overwriting data'
+    try:
+        print 'Reading existing values...'
+        preview = 3
+        existingValues = GetGSheetsRange(service, spreadsheetId, sheet,preview,
+                                        quiet=quiet)
+        print 'Emptying table...'
+        emptyValues = MakeEmptyTable(existingValues)
+        WriteGSheetsRange(service, spreadsheetId, sheet, emptyValues,
+                            quiet=quiet)
+        print 'Uploading new data...'
+        WriteGSheetsRange(service, spreadsheetId, sheet, newValues,
+                           value_input_option='USER_ENTERED',quiet=quiet)
+        print 
+        print 'Done.'
+        
+    except google_errors.HttpError as h:
+        print 'ERROR'
+        print ' - Http connection to Google Sheets failed!'
+        print ' - %s' % h
+    print
+    print
 
 def DeleteGSheetsRange(service, spreadsheetId, sheetId,
-                       startIndex, endIndex, dimension='COLUMNS'):
+                       startIndex, endIndex, dimension='COLUMNS', quiet=True):
     """Delete a range from a spreadsheet
     Note that references to cells which are deleted are not moved
     but become invlaid, as "=#REF!".
@@ -228,9 +311,9 @@ def DeleteGSheetsRange(service, spreadsheetId, sheetId,
     body = { "requests": [ {
                             "deleteDimension" : {
                                 "range":{
-                                  "sheetId": 0, # id  taken from gid=...
+                                  "sheetId": sheetId, # id  taken from gid=...
                                   "dimension": dimension,
-                                  "startIndex": startIndex,
+                                  "startIndex": startIndex, # starting from 0
                                   "endIndex": endIndex
                                   #otherwise read from sheet first
                                 }
@@ -242,60 +325,41 @@ def DeleteGSheetsRange(service, spreadsheetId, sheetId,
                   spreadsheetId=spreadsheetId,
                   body=body).execute()
 
-    print 'DELETE RESULT'
-    print
-    for k, v in result.iteritems():
-        print '%s: %s' % (k, v)
-    print
-    print
-
-###############################################################################
-# Push data to Google Sheets from CSV
-###############################################################################
-
-def Main():
-    """initially just hard code to write the campaign report"""
-    # setup
-    service = GetService()
-
-    # read all sheets data from config
-    sheetJobs = config.sheetUploadInstructions
-
-    # iterate through job titles
-    for title in sheetJobs.keys():
-        uploadSheet = sheetJobs[title] # as ['campaign_report']
-
-        # store data for specified CSV name
-        fileName = ReadCSV(uploadSheet['sheetInput'])
-        CSVdata = ReadCSV(fileName)
-
-        OverwriteCells(service, uploadSheet['spreadsheetId'],
-                       uploadSheet['sheetName'], CSVdata)
+    if not quiet:
+        print 'DELETE RESULT'
+        for k, v in result.iteritems():
+            print '%s: %s' % (k, v)
+        print
 
 def Test():
     """run this script using test() for a demo of this script.
     Hardcoded values are used for data and sheet id"""
 
+    #https://docs.google.com/spreadsheets/d/1CR_8w8ZYeu8gD9X3UwwtuZA2djj5jCpNSFycNSXM_GY/edit#gid=1608909088
     # setup
-    service = GetService()
+    
+    service = GetService() 
+    # - preface as upload.GetService() if use in another file
 
     # doc: db_adwords
-    spreadsheetId = '1d9Cb9-_-XTYHevDHUfVvQAZjJY9EN6yeZmEcR4DsFVw'
+    spreadsheetId = '1CR_8w8ZYeu8gD9X3UwwtuZA2djj5jCpNSFycNSXM_GY'
 
     # READ
     read_sheet = 'Sheet2' # please make sure this exists first
     read_data = GetGSheetsRange(service, spreadsheetId, read_sheet,
-                    printData=True)
+            quiet=False)
     print len(read_data)
 
     # WRITE
     write_sheet = 'Sheet1'
     newValues = [['a','bbbb','ccc'], ['235235'],['325235235','dgdfg','fdgfdg']]
-    OverwriteCells(service, spreadsheetId, write_sheet, newValues)
+    OverwriteCells(service, spreadsheetId, write_sheet, newValues,
+        quiet=False)
 
     # DELETE
-    DeleteSheetId = 2127202842
-    DeleteGSheetsRange(service, spreadsheetId, DeleteSheetId, 1, 3)
+    DeleteSheetId = 1608909088
+    DeleteGSheetsRange(service, spreadsheetId, DeleteSheetId, 0, 3,
+        quiet=False)
 
-if __name__ == '__main__':
-    Main()
+if __name__ == '__main__':    
+    Test() 
