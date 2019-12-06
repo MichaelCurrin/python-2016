1624007b4f7bb76d304923b64de73c3dd8916d07
michaelcurrin <michael.currin@gmail.com>
2019-12-06 13:51:52 +0200

    docs: Remove line

--- python-social-data/top_tweeters.py
+++ python-social-data/top_tweeters.py
@@ -3,8 +3,6 @@
 """
 Created on Fri Nov 25 23:12:33 2016
 
-@author: michaelcurrin
-
 Get the handles of top users on twitter
 by scraping data from four top 100 lists from socialblade.com
 Create a list of unique handles across the lists. 
