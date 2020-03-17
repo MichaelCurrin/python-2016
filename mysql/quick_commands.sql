2cf32edae52409292941693dfd133993a8b7c3eb
Michael Currin <michael.currin@gmail.com>
2020-03-17 09:07:59 +0200

    style: Update SQL script

--- mysql/quick_commands.sql
+++ mysql/quick_commands.sql
@@ -1,64 +1,60 @@
 /* I picked up a few short handy commands which I want to share with others
 I tested them on MySQLWorkbench*/
 
--- ####################
 -- count rows in table
--- ####################
 
-SELECT COUNT(*) FROM tbl_company
+SELECT COUNT(*)
+FROM tbl_company;
+
+SELECT SUM(TABLE_ROWS)
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'db_name';
 
 
-SELECT SUM(TABLE_ROWS) 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_SCHEMA = 'db_cipla_test3';
-
-
--- ####################
 -- selecting a database
--- ####################
 
 -- double-click on db name to select as default
 -- OR "USE"
-USE db_connectors_cipla;
+USE db_name;
 SELECT COUNT(*) from information_schema.tables
-WHERE table_type = 'base table'
+WHERE table_type = 'base table';
 
 
--- ####################
 -- Get Columns
--- ####################
+-- ###################
 
 -- get distinct list of columns
 SELECT DISTINCT (COLUMN_NAME)
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_SCHEMA='db_connectors_cipla'
-    ORDER BY COLUMN_NAME;
-    
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='db_name'
+ORDER BY COLUMN_NAME;
 
--- ####################
+
 -- Count rows in tables
--- ####################
 
 -- show number of rows in each table for a DB
 
-SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_ROWS 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE  TABLE_SCHEMA LIKE 'db_cipla_test_' and TABLE_ROWS > 0 
-ORDER BY `TABLES`.`TABLE_NAME` ASC, `TABLES`.`TABLE_SCHEMA`  ASC
+SELECT
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    TABLE_ROWS
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA LIKE 'db_name' and TABLE_ROWS > 0
+ORDER BY
+    `TABLES`.`TABLE_NAME` ASC,
+    `TABLES`.`TABLE_SCHEMA`  ASC;
 
--- ####################
 -- show variables
--- ####################
 
 SHOW SESSION variables
 
---
 
 -- Databases
 SHOW databases;
 
 -- Users and host
-SELECT user,host FROM mysql.user;
+SELECT user, host
+FROM mysql.user;
 
 -- Grants
 SHOW GRANTS;
@@ -66,15 +62,11 @@ GRANT ALL PRIVILEDGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
 GRANT PROX ON "@" TO 'root'@'localhost' WITH GRANT OPTION
 
 
--- ####################
 -- Get date range
--- ####################
 
 -- Show the first and last dates in a table's timestamp column
 
-SELECT 
-MIN(timestamp) AS 'Earliest date',
-MAX(timestamp) AS 'Latest date'	
-FROM tbl_socialposts; 
-
-
+SELECT
+    MIN(timestamp) AS 'Earliest date',
+    MAX(timestamp) AS 'Latest date'
+FROM tbl_socialposts;
