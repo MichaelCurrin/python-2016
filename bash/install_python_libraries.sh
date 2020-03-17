314c47892ad4c059d498ef563759bf476c5d9c47
Michael Currin <michael.currin@gmail.com>
2020-03-17 09:11:43 +0200

    docs: Update SH script

--- bash/install_python_libraries.sh
+++ bash/install_python_libraries.sh
@@ -1,14 +1,4 @@
-#!/bin/sh
-
-# I use Python 2.7 and Anaconda as my basic environment.
-# I've intalled a libraries to get me access to certain social and Google APIs and to MySQL.
-# I'd advise you to install these one at a time and also in a cloned environment. I've had an issue before
-# installing gdata library (not shown here) - it caused issues with another library so I had to start over
-# with a new installed environment.
-
-############################################################################################################
-# PIP
-############################################################################################################
+# Update: Some of these should done inside a virtual environment but here I ran them all globally.
 
 # Install PIP
 # which is a recursive acronym which stands for Pip Install Programs
@@ -17,18 +7,14 @@
 
 sudo easy_install pip
 
-############################################################################################################
-Google Analytics
-############################################################################################################
+# Google Analytics
 
 # install Google Analytics API and a few related libraries
 # (alternatively download  with your browser and install that)
 
 pip install --upgrade google-api-python-client
 
-############################################################################################################
 # OAuth
-############################################################################################################
 
 # install OAuth2
 # The oauth2 part is needed for Twitter API
@@ -36,16 +22,13 @@ pip install --upgrade google-api-python-client
 
 pip install oauth2
 
-############################################################################################################
 # MySQLdb
-############################################################################################################
-
 
 # I get an error on the recommend method of $ pip install MySQL-python
 #
 # - Could not find a version that satisfies the requirement MySQLdb (from versions: ) No matching distribution
 #    found for MySQLdb
-# - Command "python setup.py egg_info" failed with error code 1 in 
+# - Command "python setup.py egg_info" failed with error code 1 in
 #    /private/var/folders/n1/szptw8v90g17xgl59h2mj1jm0000gn/T/pip-build-yuUPhe/MySQL-python/
 
 # After much searching I found this alternative:
@@ -60,15 +43,10 @@ export PATH=$PATH:/usr/local/mysql/bin/
 # - EnvironmentError: mysql_config not found
 sudo pip install MySQL-python
 
-############################################################################################################
 # Facebook Ads
-############################################################################################################
 
 pip install facebookads
 
-
-############################################################################################################
 # Google Adwords
-############################################################################################################
 
 sudo pip install googleads
